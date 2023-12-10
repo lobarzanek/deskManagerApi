eb48b37aa1770d9318d7e4742f0798bb02c7653a
lobarzanek <lukiob@gmail.com>
2023-12-10 15:31:08 +0100

    Added mapping profiles

--- DTO/Create/CreateBrandDto.cs
+++ DTO/Create/CreateBrandDto.cs
@@ -1,6 +1,14 @@
-﻿namespace deskManagerApi.DTO.Create
+﻿using deskManagerApi.Models;
+using System.ComponentModel.DataAnnotations;
+
+namespace deskManagerApi.DTO.Create
 {
     public class CreateBrandDto
     {
+        #region Properties
+
+        public string Name { get; set; }
+
+        #endregion
     }
 }
--- Helpers/MapingProfiles.cs
+++ Helpers/MapingProfiles.cs
@@ -1,6 +1,25 @@
-﻿namespace deskManagerApi.Helpers
+﻿using AutoMapper;
+using deskManagerApi.DTO.Create;
+using deskManagerApi.DTO.Get;
+using deskManagerApi.DTO.Update;
+using deskManagerApi.Models;
+
+namespace deskManagerApi.Helpers
 {
-    public class MapingProfiles
+    public class MapingProfiles : Profile
     {
+
+        public MapingProfiles()
+        {
+            //Models to DTO
+            CreateMap<Brand, GetBrandDto>();
+
+            //Create DTO to Model
+            CreateMap<CreateBrandDto, Brand>();
+
+            //Update DTO to Model
+            CreateMap<UpdateBrandDto, Brand>();
+        }
+        
     }
 }
