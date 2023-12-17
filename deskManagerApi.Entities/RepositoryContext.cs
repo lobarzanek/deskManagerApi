030a10cff334be3528aea92c5bcf92c15a140185
lobarzanek <lukiob@gmail.com>
2023-12-17 17:04:51 +0100

    Addes Db tables

--- deskManagerApi.Entities/RepositoryContext.cs
+++ deskManagerApi.Entities/RepositoryContext.cs
@@ -16,6 +16,17 @@ namespace deskManagerApi.Entities
         {
         }
         public DbSet<Brand> Brands { get; set; }
-        public DbSet<Brand> Buildings { get; set; }
+        public DbSet<Building> Buildings { get; set; }
+        public DbSet<Desk> Desks { get; set; }
+        public DbSet<DeskStatus> DeskStatuses { get; set; }
+        public DbSet<DesksTeams> DesksTeams { get; set; }
+        public DbSet<Floor> Floors { get; set; }
+        public DbSet<Issue> Issues { get; set; }
+        public DbSet<IssueHistory> IssueHistories { get; set; }
+        public DbSet<Item> Items { get; set; }
+        public DbSet<Room> Rooms { get; set; }
+        public DbSet<Team> Teams { get; set; }
+        public DbSet<User> Users { get; set; }
+
     }
 }
