75b14a99ad02916e72e62dfe8e5f98b93a41d42b
lobarzanek <lukiob@gmail.com>
2023-12-27 20:38:09 +0100

    Update delete behaviors

--- deskManagerApi.Entities/RepositoryContext.cs
+++ deskManagerApi.Entities/RepositoryContext.cs
@@ -28,5 +28,81 @@ namespace deskManagerApi.Entities
         public DbSet<Team> Teams { get; set; }
         public DbSet<User> Users { get; set; }
 
-    }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            //Desk
+            modelBuilder.Entity<Desk>()
+                .HasOne(e => e.Room)
+                .WithMany(e => e.Desks)
+                .HasForeignKey(e => e.RoomId)
+                .OnDelete(DeleteBehavior.SetNull);
+
+            modelBuilder.Entity<Desk>()
+                .HasOne(e => e.Status)
+                .WithMany(e => e.Desks)
+                .HasForeignKey(e => e.StatusId)
+                .OnDelete(DeleteBehavior.SetNull);
+
+            //Floor
+            modelBuilder.Entity<Floor>()
+                .HasOne(e => e.Building)
+                .WithMany(e => e.Floors)
+                .HasForeignKey(e => e.BuildingId)
+                .OnDelete(DeleteBehavior.SetNull);
+
+            //Issue
+            modelBuilder.Entity<Issue>()
+                .HasOne(e => e.Desk)
+                .WithMany(e => e.Issues)
+                .HasForeignKey(e => e.DeskId)
+                .OnDelete(DeleteBehavior.SetNull);
+
+            modelBuilder.Entity<Issue>()
+                .HasOne(e => e.Reporter)
+                .WithMany(e => e.Issues)
+                .HasForeignKey(e => e.ReporterId)
+                .OnDelete(DeleteBehavior.SetNull);
+
+            //Issue History
+            modelBuilder.Entity<IssueHistory>()
+                .HasOne(e => e.User)
+                .WithMany(e => e.IssueHistories)
+                .HasForeignKey(e => e.ChangedBy)
+                .OnDelete(DeleteBehavior.SetNull);
+
+            //Item
+            modelBuilder.Entity<Item>()
+                .HasOne(e => e.Owner)
+                .WithMany(e => e.Items)
+                .HasForeignKey(e => e.OwnerId)
+                .OnDelete(DeleteBehavior.SetNull);
+
+            modelBuilder.Entity<Item>()
+                .HasOne(e => e.Brand)
+                .WithMany(e => e.Items)
+                .HasForeignKey(e => e.BrandId)
+                .OnDelete(DeleteBehavior.SetNull);
+
+            modelBuilder.Entity<Item>()
+                .HasOne(e => e.Desk)
+                .WithMany(e => e.Items)
+                .HasForeignKey(e => e.DeskId)
+                .OnDelete(DeleteBehavior.SetNull);
+
+            //Room
+            modelBuilder.Entity<Room>()
+                .HasOne(e => e.Floor)
+                .WithMany(e => e.Rooms)
+                .HasForeignKey(e => e.FloorId)
+                .OnDelete(DeleteBehavior.SetNull);
+
+            //User
+            modelBuilder.Entity<User>()
+                .HasOne(e => e.Team)
+                .WithMany(e => e.Users)
+                .HasForeignKey(e => e.TeamId)
+                .OnDelete(DeleteBehavior.SetNull);
+        }
+
+        }
 }
