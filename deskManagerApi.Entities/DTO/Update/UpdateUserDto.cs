d6ffe65a0ddc2ae16d00afb24631724f85cd8eb8
lobarzanek <lukiob@gmail.com>
2023-12-27 20:07:46 +0100

    Update user DTOs

--- deskManagerApi.Entities/DTO/Update/UpdateUserDto.cs
+++ deskManagerApi.Entities/DTO/Update/UpdateUserDto.cs
@@ -1,12 +1,56 @@
-﻿using System;
+﻿using deskManagerApi.Models;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace deskManagerApi.Entities.DTO.Update
 {
-    internal class UpdateUserDto
+    public class UpdateUserDto
     {
+        #region Properties and indexers
+
+        /// <summary>
+        /// Gets or sets the ID value of user.
+        /// </summary>
+        [Key]
+        public int Id { get; set; }
+
+        /// <summary>
+        /// Gets or sets the firstname value of user.
+        /// </summary>
+        [Required(ErrorMessage = "Firstname is required")]
+        [StringLength(20, ErrorMessage = "Firstname can't be longer than 20 characters")]
+        public string FirstName { get; set; }
+
+        /// <summary>
+        /// Gets or sets the lastname value of user.
+        /// </summary>
+        [Required(ErrorMessage = "Lastname is required")]
+        [StringLength(20, ErrorMessage = "Lastname can't be longer than 20 characters")]
+        public string LastName { get; set; }
+
+        /// <summary>
+        /// Gets or sets the login value of user.
+        /// </summary>
+        [Required(ErrorMessage = "Login is required")]
+        [StringLength(20, ErrorMessage = "Login can't be longer than 20 characters")]
+        public string Login { get; set; }
+
+        /// <summary>
+        /// Gets or sets the role value of user.
+        /// </summary>
+        public Role RoleId { get; set; }
+
+        /// <summary>
+        /// Gets or sets the value of team ID.
+        /// </summary>
+        [ForeignKey("Team")]
+        public int? TeamId { get; set; }
+
+        #endregion
     }
 }
